b9e56746427d824481fd10f2a76a1ce05afbff04
Shavit Tzuriel <stmmsn@gmail.com>
2016-07-04 05:37:03 -0400

    🎈

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template import loader
 
+import re
+
 from .models import Post
 from .models import WPPost
 from .models import WpPostmeta
@@ -71,7 +73,7 @@ def show(request, slug):
             ON thumbnail.post_id = wp_posts.id
         WHERE wp_posts.post_type = "post"
             AND wp_posts.post_status = "publish"
-            AND post_name=post_name
+            AND post_name = %(post_name)s
         LIMIT 1
     """
     recipe = WPPost.objects.raw(query, {
@@ -81,10 +83,56 @@ def show(request, slug):
     if recipe == list():
         return redirect('/recipes')
 
+    # content filters
+    recipe = recipe[0]
+    recipe.body  = bodyFilters(recipe.body)
+
+    # query related recipes
+    query = """
+        SELECT
+            ID, post_title,post_status,thumbnail.meta_key,thumbnail.meta_value
+        FROM wp_posts
+        RIGHT JOIN
+            (
+                SELECT
+                    m.post_id,m.meta_id,t.meta_key,t.meta_value
+                FROM wp_postmeta AS t
+                    LEFT JOIN wp_postmeta AS m
+                    ON m.meta_value = t.post_id
+                WHERE t.meta_key = "_wp_attached_file"
+                    AND m.meta_key = "_thumbnail_id"
+            ) AS thumbnail
+            ON thumbnail.post_id = wp_posts.id
+        WHERE wp_posts.post_type = "post"
+            AND wp_posts.post_status = "publish"
+        ORDER BY
+            wp_posts.post_date_gmt DESC
+        LIMIT 6
+    """
+    related_recipes = WPPost.objects.raw(query)
+
     template = loader.get_template('recipes/show.html')
     context = {
-        'title': recipe[0].title,
-        'recipe': recipe[0]
+        'title': recipe.title,
+        'recipe': recipe,
+        'related_recipes': related_recipes
     }
 
     return HttpResponse(template.render(context, request))
+
+
+#
+#   Helpers
+#
+
+def convertToYouTubeCode(body):
+    match = re.search(r'^(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?$', body)
+    if not match:
+        return body
+    embed_code = 'https://www.youtube.com/embed/{}'.format(match.group(2))
+    converted_body = '<iframe width="560" height="315" src="{}" frameborder="0" allowfullscreen="allowfullscreen" ></iframe>'.format(embed_code)
+    return converted_body+body
+
+def bodyFilters(body):
+    body = convertToYouTubeCode(body)
+    return body
