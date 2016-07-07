932c82aee776afcfab6ca2f4c60f1f8704f13a96
Shavit Tzuriel <stmmsn@gmail.com>
2016-07-07 18:59:34 -0400

    🏞

--- recipes/settings.py
+++ recipes/settings.py
@@ -188,7 +188,8 @@ CMS_TEMPLATES = (
     ## Customize this
     ('fullwidth.html', 'Fullwidth'),
     ('sidebar_left.html', 'Sidebar Left'),
-    ('sidebar_right.html', 'Sidebar Right')
+    ('sidebar_right.html', 'Sidebar Right'),
+    ('homepage.html', 'Homepage'),
 )
 
 
