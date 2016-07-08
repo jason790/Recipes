c5618165ff982179c45af8cff81bd42d1c1c83b6
Shavit Tzuriel <stmmsn@gmail.com>
2016-07-08 17:36:33 -0400

    📿

--- recipes/settings.py
+++ recipes/settings.py
@@ -73,7 +73,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+# STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+STATIC_ROOT = os.path.join(DATA_DIR, 'recipes', 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'recipes', 'static'),
